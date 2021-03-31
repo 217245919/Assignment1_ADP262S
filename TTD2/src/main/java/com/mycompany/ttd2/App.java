30bdd2bc952738d912fb171ef180942f6cd6454a
Uchiarain <81322651+Uchiarain@users.noreply.github.com>
2021-03-31 20:33:22 +0200

    Add files via upload

---
+++ TTD2/src/main/java/com/mycompany/ttd2/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.ttd2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   
+    public double equal(double number1 , double number2){
+        
+        double equality = number1 + number2;
+        
+        System.out.println("equal to " + equality);
+        return equality;
+    }
+}
